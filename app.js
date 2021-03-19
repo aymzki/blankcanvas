6ac7306139fa60fa1ac945b6423b0d840bfc256e
Alissa Yamazaki <alissa.yamazaki@gmail.com>
2021-03-19 13:14:57 -0400

    styling

---
+++ app.js
@@ -0,0 +1,10 @@
+//let test = document.getElementById("circle");
+
+//test.addEventListener("mouseover", function (changecolor) 
+//{changecolor.body.style.backgroundImage = 
+//   'linear-gradient('+ #ff66e4 +','+ #2af5fc +')';
+//});
+
+function bgChange(bg) {
+    document.body.style.background = bg;
+  }
